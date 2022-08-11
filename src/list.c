d6fc6edeeb4dd3e2a3b8f5eec70117ea58da334a
Kwaltz Buck <kwaltzbu@se-b5.nsk.21-school.ru>
2022-08-11 19:35:25 +0700

    lol

--- src/list.c
+++ src/list.c
@@ -6,52 +6,64 @@
 #include "door_struct.h"
 
 struct node* init(struct door* dp) {
-  struct node* new_node = malloc(sizeof(struct node));
-  new_node->d = dp;
-  new_node->next = NULL;
+  struct node* new_node = NULL;
+  if (dp != NULL) {
+    new_node = malloc(sizeof(struct node));
+    new_node->d = dp;
+    new_node->next = NULL;
+  }
   return new_node;
 }
 struct node* add_door(struct node* el, struct door* dp) {
-  struct node* new_node = init(dp);
-  new_node->next = el->next;
-  el->next = new_node;
+  struct node* new_node = NULL;
+  if (el != NULL && dp != NULL) {
+    new_node = init(dp);
+    new_node->next = el->next;
+    el->next = new_node;
+  }
   return new_node;
 }
 struct node* find_door(int door_id, struct node* root) {
   struct node* search = root;
-  while (search != NULL && search->d->id != door_id) {
-    search = search->next;
+  if (root != NULL) {
+    while (search != NULL && search->d->id != door_id) {
+      search = search->next;
+    }
   }
   return search;
 }
 struct node* remove_door(struct node* elem, struct node* root) {
-  struct node* prev = root;
-  struct node* n = root;
-  while (n != elem && n != NULL) {
-    prev = n;
-    n = n->next;
-  }
-  if (n != NULL) {
-    if (n != root) {
-      prev->next = n->next;
-      free(n);
-    } else {
-      if (root->next != NULL)
-        root = root->next;
-      else
-        root = NULL;
-      free(n);
+  if (root != NULL && elem != NULL) {
+    struct node* prev = root;
+    struct node* n = root;
+    while (n != elem && n != NULL) {
+      prev = n;
+      n = n->next;
+    }
+    if (n != NULL) {
+      if (n != root) {
+        prev->next = n->next;
+        free(n);
+      } else {
+        if (root->next != NULL)
+          root = root->next;
+        else
+          root = NULL;
+        free(n);
+      }
     }
   }
   return root;
 }
 void destroy(struct node* root) {
-  struct node* destroyer = root;
+  if (root != NULL) {
+    struct node* destroyer = root;
 
-  while (destroyer != NULL) {
-    root = root->next;
-    free(destroyer);
-    destroyer = root;
+    while (destroyer != NULL) {
+      root = root->next;
+      free(destroyer);
+      destroyer = root;
+    }
   }
 }
 
